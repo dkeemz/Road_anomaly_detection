ec70716b9a56cdb4cc946b692016eca0e6084a7c
Abdulhakeem Adamu Bakari <61731545+dkeemz@users.noreply.github.com>
2021-09-30 01:10:46 +0100

    Add files via upload

---
+++ pothole_detection_pc.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+import glob
+import random
+
+
+# Load cv2 dnn
+net = cv2.dnn.readNet("yolov3_4.weights", "yolov3_testing.cfg")
+
+# custom object name
+classes = ["Pothole"]
+
+# Images path
+
+images_path = glob.glob(r"C:\Users\WHAXEERI\Desktop\Finished\PotholeCoco\Dataset\train\*.jpg")
+
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+# Function for detection
+def func(img):
+    ret,img = cap.read()    
+    img = cv2.resize(img, None, fx=0.4, fy=0.4)
+    height, width, channels = img.shape
+
+    # Detecting objects
+    # blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (32, 32), (0, 0, 0), True, crop=False)
+
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    # Showing output on the screen
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.01:
+                # if Object detected
+                print(class_id)
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # Bounding box coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    print(indexes)
+    font = cv2.FONT_HERSHEY_PLAIN
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            color = colors[class_ids[i]]
+            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(img, label, (x, y + 30), font, 1.0, color, 2)
+    return img
+
+if __name__ == '__main__':
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret,img = cap.read()
+        func(img)
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    img.release()
+    cv2.destroyAllWindows()
